9a178b69affdcbad17394d4de6ca6d685671c466
Matheus Wendel <matheus_wendel@outlook.com>
2020-12-05 16:56:30 -0300

    remove import

--- src/main/java/fatec/controller/CompraController.java
+++ src/main/java/fatec/controller/CompraController.java
@@ -3,7 +3,6 @@ package fatec.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fatec.model.Compra;
 @RestController
 @RequestMapping("/compra")
 public class CompraController  {
